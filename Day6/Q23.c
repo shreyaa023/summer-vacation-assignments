08926d835a5b2e5bc04535672c2c17453c9c225f
shreyaa023 <shreyagupta022006@gmail.com>
2026-06-10 17:31:52 +0530

    Add program to count set bits in a number

---
+++ Day6/Q23.c
@@ -0,0 +1,24 @@
+/*Write a program to Count set bits in a number*/
+#include <stdio.h>
+
+int countSetBits(int n) {
+    int count = 0;
+
+    while (n > 0) {
+        count += n & 1;  // Check if the last bit is 1
+        n >>= 1;         // Right shift by 1
+    }
+
+    return count;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Number of set bits = %d\n", countSetBits(num));
+
+    return 0;
+}
