4db7d6f360f26d80cc27a068b28612d998baea69
shreyaa023 <shreyagupta022006@gmail.com>
2026-06-10 17:35:13 +0530

    Add recursive factorial program in C

---
+++ Day7/Q25.c
@@ -0,0 +1,24 @@
+/*Write a program to Recursive factorial*/
+#include <stdio.h>
+
+// Recursive function to find factorial
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d = %lld\n", num, factorial(num));
+
+    return 0;
+}
