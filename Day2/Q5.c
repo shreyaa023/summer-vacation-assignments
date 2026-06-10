332ef2b8006eecb8fb64a49f539b5a8fe4e17d39
shreyaa023 <shreyagupta022006@gmail.com>
2026-06-10 17:01:04 +0530

    Implement sum of digits program in C
    This program calculates the sum of the digits of a given number by repeatedly extracting the last digit and adding it to a cumulative sum.

---
+++ Day2/Q5.c
@@ -0,0 +1,19 @@
+/*Write a program to Find sum of digits of a number*/
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        digit = num % 10;   // Get last digit
+        sum = sum + digit;  // Add digit to sum
+        num = num / 10;     // Remove last digit
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
