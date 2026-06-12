2deb8ab3856598f214aa07b8d02ae5c321b810de
shreyaa023 <shreyagupta022006@gmail.com>
2026-06-12 21:00:58 +0530

    Add program to find maximum of two numbers

---
+++ Day11/Q42.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Function to find maximum of two numbers
+int maximum(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Maximum number = " << maximum(num1, num2);
+
+    return 0;
+}
