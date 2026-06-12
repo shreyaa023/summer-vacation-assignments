b3ba45b989ab4db1c9f09f9f0c1f1c4d05ee5b9a
shreyaa023 <shreyagupta022006@gmail.com>
2026-06-12 21:04:27 +0530

    Add factorial function and main program

---
+++ Day11/Q44.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Function to find factorial
+long long factorial(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << num << " = " << factorial(num) << endl;
+    }
+
+    return 0;
+}
